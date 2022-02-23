fc1d5a415eec6a2b9d5c06c696d532c03078788b
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-02-23 10:55:15 +0100

    typing home component

--- apps/meso/src/app/home/home.component.ts
+++ apps/meso/src/app/home/home.component.ts
@@ -1,6 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientRequest } from 'http';
 
-const tango = [];
+interface BaseEntity {
+  id: string | null
+}
+
+interface Client extends BaseEntity {
+  firstName: string;
+  lastName: string;
+  company: string
+}
+
+const peter: Client = {
+  id: '1',
+  firstName: 'Peter',
+  lastName: 'Porker',
+  company: 'Acme, Inc'
+}
+
+const john: Client = {
+  id: '2',
+  firstName: 'John',
+  lastName: 'Doe',
+  company: 'N/A'
+}
+
+const clients: Client[] = [
+  peter,
+  john
+]
+
+const tango = clients;
 
 @Component({
   selector: 'fem-home',
